0952db97ccf4bc64e51dc665014e72a864348615
hitozen2t@gmail.com <68021686+hitozen2t@users.noreply.github.com>
2020-07-17 08:17:38 +0700

    update

--- src/githubtutorial/GithubTutorial.java
+++ src/githubtutorial/GithubTutorial.java
@@ -18,7 +18,14 @@ public class GithubTutorial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //hy vong ham tra ve 120 neu tinh 5!
         System.out.println("5! = "+computeFactorial(5));
+        //hy vong ham tra ve 1 neu tinh 1!
+        System.out.println("1!="+computeFactorial(1));
+        //expected 1 if 0!
+        System.out.println("0!="+computeFactorial(0));
+        //expected Exception if cf(-5)
+        System.out.println("-5!="+computeFactorial(-5));
     }
     
 }
---
+++ test/tdd/MathUtilTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static nhan.util.MathUtil.computeFactorial;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtilTest {
+    @Test
+    
+    public void testSucessesfulCases(){
+        assertEquals(120, computeFactorial(5));
+        assertEquals(5040, computeFactorial(7));
+        assertEquals(1,computeFactorial(1));
+        assertEquals(1, computeFactorial(0));
+        assertEquals(2, computeFactorial(2));
+// t ki vong co 
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFailedCases(){
+        
+    }
+    
+}
